5c890e8d67406645dc6e4dbaffde6fbfa3f6a7af
devon <devon@affirm.com>
2016-12-26 14:40:45 -0500

    Remove Stars component & reducer

--- src/app/containers/index.tsx
+++ src/app/containers/index.tsx
@@ -4,4 +4,3 @@ export { App } from './App';
 export { Home } from './Home';
 export { About } from './About';
 export { Counter } from './Counter';
-export { Stars } from './Stars';
--- src/app/redux/reducers.ts
+++ src/app/redux/reducers.ts
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { counterReducer } from './modules/counter';
-import { starsReducer } from './modules/stars';
 const { reducer } = require('redux-connect');
 
 const rootReducer: Redux.Reducer = combineReducers({
   routing: routerReducer,
   counter: counterReducer,
-  stars: starsReducer,
   reduxAsyncConnect: reducer,
 });
 
