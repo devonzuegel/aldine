bd218dc61f943f33487a4a0695710c8c7c32c782
devon <devon@affirm.com>
2016-12-27 22:59:31 -0500

    Succesfully load json, update sublime-project

--- src/app/containers/Home/index.tsx
+++ src/app/containers/Home/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 const s = require('./style.css');
+const json = require('./test.json');
 
 class Home extends React.Component<any, any> {
   public render() {
@@ -7,6 +8,7 @@ class Home extends React.Component<any, any> {
       <div className={s.home}>
         <img src={require('./barbar.png')} />
         <p>Hello!</p>
+        <pre>{JSON.stringify(json, null, 2)}</pre>
       </div>
     );
   }
