15b0484d76e4f647375638d8ce1fca1fa166877f
devon <devon@affirm.com>
2016-12-30 02:17:04 -0500

    Remove Barbar img

--- src/app/containers/Home/index.tsx
+++ src/app/containers/Home/index.tsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
 const s = require('./style.css');
-const md = require('./test.md?tagged-prose').default;
 
 class Home extends React.Component<any, any> {
   public render() {
     return (
       <div className={s.home}>
-        <img src={require('./barbar.png')} />
         <p>Hello!</p>
-
-        <div>{md(s.noun)}</div>
       </div>
     );
   }
