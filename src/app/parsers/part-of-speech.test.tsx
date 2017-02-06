da4163bebf579cbf499bf820b94e8eb1ca7c0c9c
devon <devon@affirm.com>
2017-02-06 09:30:51 -0800

    Implement part-of-speech parser

--- src/app/parsers/part-of-speech.test.tsx
+++ src/app/parsers/part-of-speech.test.tsx
@@ -1,18 +1,26 @@
-import * as React from 'react'
 import { expect } from 'chai'
 
 import tagPOS from './part-of-speech'
 
-const plainTxt = 'This is some plain text'
-
 describe('Tagged Prose', () => {
-  it('..', () => {
-    expect(tagPOS(plainTxt)).eql([
+  it('tags a simple sentence', () => {
+    expect(tagPOS('This is some plain text')).eql([
       { word: 'This',  pos: 'DT'  },
       { word: 'is',    pos: 'VBZ' },
       { word: 'some',  pos: 'DT'  },
       { word: 'plain', pos: 'JJ'  },
       { word: 'text',  pos: 'NN'  },
+    ])
+  })
+
+  it('mis-tags a garden path sentence', () => {
+    expect(tagPOS('The old man the boat.')).eql([
+      { word: 'The',  pos: 'DT' },
+      { word: 'old',  pos: 'JJ' },
+      { word: 'man',  pos: 'NN' },
+      { word: 'the',  pos: 'DT' },
+      { word: 'boat', pos: 'NN' },
+      { word: '.',    pos: '.'  },
     ])
   })
 })
