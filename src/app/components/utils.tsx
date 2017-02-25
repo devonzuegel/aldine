2f8bee661e7e0ebd2877d954b7e9ea4681b40b5b
devon <devon@affirm.com>
2017-02-25 15:50:11 -0800

    Add component/Section and components/utils

---
+++ src/app/components/utils.tsx
@@ -0,0 +1,22 @@
+import * as R     from 'ramda'
+import * as React from 'react'
+
+export const attitudes = [ 'neutral', 'positive', 'negative' ]
+export const emphases  = [ 'primary', 'secondary' ]
+export const examples  = (Component) => (
+  attitudes.map((attitude: string, i: number) =>
+    emphases.map((emphasis: string, j: number) =>
+      <Component key={`${i}-${j}`} {...{ attitude, emphasis }} />
+    )
+  )
+)
+
+export const propTypes = (props) => R.merge(props, {
+  attitude: React.PropTypes.oneOf(attitudes),
+  emphasis: React.PropTypes.oneOf(emphases),
+})
+
+export const defaultProps = (props = {}) => R.merge(props, {
+  emphasis: 'primary',
+  attitude: 'neutral',
+})
