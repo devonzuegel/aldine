518e87b6e51c8d9cf15d2f15fbd1c131158a6134
devon <devon@affirm.com>
2017-03-05 22:31:37 -0800

    Rename modules/Home to modules/ThemeChanger

--- src/app/modules/ThemeChanger/index.test.tsx
+++ src/app/modules/ThemeChanger/index.test.tsx
@@ -2,39 +2,13 @@ import { expect          } from 'chai'
 import { renderComponent } from '~/components/TestHelper'
 import { ThemeChanger    } from './index'
 
-/** Mock App. State */
-const state = {
-  themeChanger: { className: 'dark-faded-plaintext' },
-}
-const styles = require('~/components/ThemeChanger/style.css')
-
-const selectorClass = `.${styles.themeSelector}`
-const props = {
-  themes: [
-    { title: 'Black background, faded plaintext', className: 'dark-faded-plaintext'  },
-    { title: 'Plain text',                        className: ''                      },
-    { title: 'Faded plaintext',                   className: 'faded-plaintext-all'   },
-    { title: 'Faded plaintext, nouns only',       className: 'faded-plaintext-nouns' },
-    { title: 'Faded plaintext, verbs only',       className: 'faded-plaintext-verbs' },
-  ],
-}
-
 describe('<ThemeChanger />', () => {
-  beforeEach(() => {
-    this.component = renderComponent(ThemeChanger, state, props)
+
+  const component = renderComponent(ThemeChanger)
+
+  it('Renders with correct style', () => {
+    const s = require('./style.css')
+    expect(component.find(s.home)).to.exist
   })
 
-  it('updates the active selector when that selector is clicked', () => {
-    const themeElems = this.component.find(selectorClass)
-    const [ first, last ] = [ themeElems.first(), themeElems.last() ]
-    const active = `${styles.themeSelector} ${styles.active}`
-
-    expect(first.props().className).to.eql(active)
-    expect(last.props().className).to.eql(styles.themeSelector)
-
-    last.simulate('click')
-
-    expect(first.props().className).to.eql(styles.themeSelector)
-    expect(last.props().className).to.eql(active)
-  })
 })
