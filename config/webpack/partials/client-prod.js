f1de2c2ca23248973fc66b5623c4e4fe0e389fb1
devon <devon@affirm.com>
2017-04-02 15:47:23 -0700

    Update client-prod for build:prod task

--- config/webpack/partials/client-prod.js
+++ config/webpack/partials/client-prod.js
@@ -1,36 +1,56 @@
-const path    = require('path')
-const webpack = require('webpack')
+const fs             = require('fs')
+const path           = require('path')
+const webpack        = require('webpack')
+const ManifestPlugin = require('webpack-manifest-plugin')
 
-module.exports = require('webpack-merge')(
+const config = require('webpack-merge')(
   require('./aliases'),
   require('./client-loaders'),
   {
-    bail: true,
-
     entry: {
-      app: './src/client.tsx',
-      vendor: require('./vendor-entry'),
+      app: [
+        './src/client.tsx',
+        './src/vendor/main.ts'
+      ]
     },
 
-    output: require('./output')({ pathinfo: false }),
+    output: {
+      path: path.resolve('./build/public'),
+      publicPath: '/public/',
+      filename: 'js/[name].[hash].js',
+    },
 
     plugins: [
-      new webpack.optimize.OccurrenceOrderPlugin(),
-      new webpack.optimize.CommonsChunkPlugin({
-        name:      'vendor',
-        filename:  'js/[name].[chunkhash].js',
-        minChunks: Infinity
-      }),
-      new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }),
-      new require('extract-text-webpack-plugin')('css/[name].[hash].css'),
-      new require('webpack-manifest-plugin')({ fileName: '../manifest.json' }),
+      new ManifestPlugin({ fileName: '../manifest.json' }),
       new webpack.DefinePlugin({
         'process.env': {
-          BROWSER:  JSON.stringify(true),
-          NODE_ENV: JSON.stringify('production')
+          BROWSER: JSON.stringify(true),
+          NODE_ENV: JSON.stringify('prod')
         }
-      })
-    ],
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    ]
   }
 )
 
+const copySync = (src, dest, overwrite) => {
+  if (overwrite && fs.existsSync(dest)) {
+    fs.unlinkSync(dest);
+  }
+  const data = fs.readFileSync(src);
+  fs.writeFileSync(dest, data);
+}
+
+const createIfDoesntExist = dest => {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest);
+  }
+}
+
+createIfDoesntExist('./build');
+createIfDoesntExist('./build/public');
+copySync('./src/assets/favicon.ico', './build/public/favicon.ico', true);
+
+
+module.exports = config
