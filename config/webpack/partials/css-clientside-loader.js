852e9563a5ca5706e6469e199db3d300512c7ea0
devon <devon@affirm.com>
2017-04-04 23:34:40 -0700

    Add ExtractTextPlugin to clientside CSS loader

--- config/webpack/partials/css-clientside-loader.js
+++ config/webpack/partials/css-clientside-loader.js
@@ -1,3 +1,4 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
 
 const projectRoot = (...absolutePath) =>
@@ -5,29 +6,12 @@ const projectRoot = (...absolutePath) =>
 
 module.exports = {
   test:    /\.css$/,
-  include: projectRoot('app'),
-  loaders: [
-    {
-      loader: 'style-loader'
-    }, {
-      loader: 'css-loader',
-      options: {
-        importLoaders:  2,
-        sourceMap:      true,
-        modules:        true,
-        localIdentName: '[local]___[hash:base64:5]',
-        includePaths:   [projectRoot('node_modules')],
-      },
-    }, {
-      loader: 'postcss-loader',
-      options: {
-        plugins: () => [
-          require('postcss-import')({ path: [ projectRoot('app') ] }),
-          require('stylelint')({ files: projectRoot('app/*.css') }),
-          require('postcss-cssnext')(),
-          require('postcss-assets')({ relative: projectRoot('app') })
-        ],
-      },
-    },
-  ],
+  loader: ExtractTextPlugin.extract({
+    fallbackLoader: 'style-loader',
+    loader: [
+      { loader: 'css-loader', options: { importLoaders: 3, sourceMap: true, modules: true } },
+      'resolve-url-loader',
+      'postcss-loader',
+    ],
+  }),
 }
